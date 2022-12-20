cfc0f7e5faad9cea049e8e7cc83850dbeb276981
FarrukhAdeel67 <m.farrukhadeel@gmail.com>
2022-12-20 23:34:05 +0500

    template created

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>ALLAH HO AKBAR</h1>
+        <h2>Prophet MUHAMMAD Peace Be Upon Him</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
